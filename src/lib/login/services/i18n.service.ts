d1cdbf3b51ea0401435da69d0a38832e3c401119
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-09-23 17:56:58 +0200

    add other exports

--- src/lib/login/services/i18n.service.ts
+++ src/lib/login/services/i18n.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { I18n } from '../../i18n';
+import { I18n } from '../i18n';
 @Injectable({ providedIn: 'root' })
 export class I18nService {
   i18n!: I18n;
--- src/public-api.ts
+++ src/public-api.ts
@@ -2,8 +2,11 @@
  * Public API Surface of keycloakify-angular
  */
 
-export * from './lib/login/directives/kc-class.directive';
 export * from './lib/login/directives/use-default-css.directive';
+export * from './lib/login/directives/kc-class.directive';
 export * from './lib/login/pipes/kcSanitize.pipe';
 export * from './lib/login/pipes/translate.pipe';
+export * from './lib/login/services/resource-injector.service'
+export * from './lib/login/services/i18n.service'
+export * from './lib/login/classes/component-reference.class'
 

360444a8bf36ca861bef14bcc253634c0adde3b7
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-09-24 21:51:32 +0000

    add delete-credential.ftl

--- projects/keycloakify-angular/src/lib/login/pages/delete-credential/delete-credential.component.ts
+++ projects/keycloakify-angular/src/lib/login/pages/delete-credential/delete-credential.component.ts
@@ -2,11 +2,14 @@ import { ChangeDetectionStrategy, Component, forwardRef, inject, input } from '@
 import { ClassKey } from 'keycloakify/login';
 import { KcContext } from 'keycloakify/login/KcContext';
 import { ComponentReference } from '../../classes/component-reference.class';
+import { TemplateComponent } from '../../containers/template.component';
+import { KcClassDirective } from '../../directives/kc-class.directive';
+import { MsgStrPipe } from '../../pipes/msg-str.pipe';
 import { KC_CONTEXT } from '../../providers/keycloakify-angular.providers';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [MsgStrPipe, TemplateComponent, KcClassDirective],
   selector: 'kc-root',
   templateUrl: 'delete-credential.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,4 +24,5 @@ export class DeleteCredentialComponent extends ComponentReference {
   kcContext = inject<Extract<KcContext, { pageId: 'delete-credential.ftl' }>>(KC_CONTEXT);
   override doUseDefaultCss = input<boolean>();
   override classes = input<Partial<Record<ClassKey, string>>>();
+  displayMessage: boolean = false;
 }

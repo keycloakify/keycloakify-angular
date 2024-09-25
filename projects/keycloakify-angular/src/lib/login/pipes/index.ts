5464f65945b52cc21c32ad39c3350d2532cc2eb0
Luca Peruzzo <luca.peruzzo@euronovate.com>
2024-09-25 15:13:38 +0200

    missing pipe export

--- projects/keycloakify-angular/src/lib/login/pipes/index.ts
+++ projects/keycloakify-angular/src/lib/login/pipes/index.ts
@@ -3,3 +3,5 @@ export * from './kcTranslate.pipe';
 export * from './input-type.pipe';
 export * from './to-array.pipe';
 export * from './to-number.pipe';
+export * from './msg-str.pipe';
+export * from './advanced-msg-str.pipe';

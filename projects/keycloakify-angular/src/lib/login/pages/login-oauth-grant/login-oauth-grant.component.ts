8e191c0a50d9e7703e06a30288a8f57861befb40
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-09-25 07:44:31 +0000

    add login-oauth-grant.ftl

--- projects/keycloakify-angular/src/lib/login/pages/login-oauth-grant/login-oauth-grant.component.ts
+++ projects/keycloakify-angular/src/lib/login/pages/login-oauth-grant/login-oauth-grant.component.ts
@@ -2,23 +2,27 @@ import { ChangeDetectionStrategy, Component, forwardRef, inject, input } from '@
 import { ClassKey } from 'keycloakify/login';
 import { KcContext } from 'keycloakify/login/KcContext';
 import { ComponentReference } from '../../classes/component-reference.class';
+import { TemplateComponent } from '../../containers/template.component';
+import { KcClassDirective } from '../../directives/kc-class.directive';
+import { AdvancedMsgStrPipe } from '../../pipes/advanced-msg-str.pipe';
+import { MsgStrPipe } from '../../pipes/msg-str.pipe';
 import { KC_CONTEXT } from '../../providers/keycloakify-angular.providers';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [KcClassDirective, TemplateComponent, MsgStrPipe, AdvancedMsgStrPipe],
   selector: 'kc-root',
   templateUrl: 'login-oauth-grant.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    {
-      provide: ComponentReference,
-      useExisting: forwardRef(() => LoginOauthGrantComponent),
-    },
-  ],
+  providers: [{ provide: ComponentReference, useExisting: forwardRef(() => LoginOauthGrantComponent) }],
 })
 export class LoginOauthGrantComponent extends ComponentReference {
   kcContext = inject<Extract<KcContext, { pageId: 'login-oauth-grant.ftl' }>>(KC_CONTEXT);
   override doUseDefaultCss = input<boolean>();
   override classes = input<Partial<Record<ClassKey, string>>>();
+  displayRequiredFields = input(false);
+  documentTitle = input<string>();
+  bodyClassName = input<string>('oauth');
+  displayInfo: boolean = false;
+  displayMessage: boolean = false;
 }

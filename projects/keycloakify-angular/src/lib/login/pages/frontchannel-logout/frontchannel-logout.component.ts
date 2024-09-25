811714fb1e07b3c273b47a88c0c77bd8a70fdbcd
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-09-25 07:18:17 +0000

    add frontchannel-logout.ftl

--- projects/keycloakify-angular/src/lib/login/pages/frontchannel-logout/frontchannel-logout.component.ts
+++ projects/keycloakify-angular/src/lib/login/pages/frontchannel-logout/frontchannel-logout.component.ts
@@ -1,12 +1,15 @@
-import { ChangeDetectionStrategy, Component, forwardRef, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, inject, input, OnInit } from '@angular/core';
 import { ClassKey } from 'keycloakify/login';
 import { KcContext } from 'keycloakify/login/KcContext';
 import { ComponentReference } from '../../classes/component-reference.class';
+import { TemplateComponent } from '../../containers/template.component';
+import { KcSanitizePipe } from '../../pipes';
+import { MsgStrPipe } from '../../pipes/msg-str.pipe';
 import { KC_CONTEXT } from '../../providers/keycloakify-angular.providers';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TemplateComponent, MsgStrPipe, KcSanitizePipe],
   selector: 'kc-root',
   templateUrl: 'frontchannel-logout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,8 +20,18 @@ import { KC_CONTEXT } from '../../providers/keycloakify-angular.providers';
     },
   ],
 })
-export class FrontchannelLogoutComponent extends ComponentReference {
+export class FrontchannelLogoutComponent extends ComponentReference implements OnInit {
   kcContext = inject<Extract<KcContext, { pageId: 'frontchannel-logout.ftl' }>>(KC_CONTEXT);
   override doUseDefaultCss = input<boolean>();
   override classes = input<Partial<Record<ClassKey, string>>>();
+  displayRequiredFields = input(false);
+  bodyClassName = input<string>();
+  displayInfo: boolean = false;
+  displayMessage: boolean = false;
+
+  ngOnInit(): void {
+    if (this.kcContext.logout.logoutRedirectUri) {
+      window.location.replace(this.kcContext.logout.logoutRedirectUri);
+    }
+  }
 }

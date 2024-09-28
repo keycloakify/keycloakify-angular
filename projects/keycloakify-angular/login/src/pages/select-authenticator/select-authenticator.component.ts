f04800321365cb8621553173327b89037bb25a09
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-09-28 10:43:04 +0200

    adds select-authenticator.ftl

--- projects/keycloakify-angular/login/src/pages/select-authenticator/select-authenticator.component.ts
+++ projects/keycloakify-angular/login/src/pages/select-authenticator/select-authenticator.component.ts
@@ -1,12 +1,17 @@
+import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, forwardRef, inject, input } from '@angular/core';
 import { KC_CONTEXT } from 'keycloakify-angular';
 import { ClassKey } from 'keycloakify/login';
 import { KcContext } from 'keycloakify/login/KcContext';
 import { ComponentReference } from '../../classes/component-reference.class';
+import { TemplateComponent } from '../../containers/template.component';
+import { KcClassDirective } from '../../directives';
+import { AdvancedMsgStrPipe } from '../../pipes';
+import { MsgStrPipe } from '../../pipes/msg-str.pipe';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TemplateComponent, MsgStrPipe, AdvancedMsgStrPipe, KcClassDirective, NgClass],
   selector: 'kc-root',
   templateUrl: 'select-authenticator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,4 +26,14 @@ export class SelectAuthenticatorComponent extends ComponentReference {
   kcContext = inject<Extract<KcContext, { pageId: 'select-authenticator.ftl' }>>(KC_CONTEXT);
   override doUseDefaultCss = input<boolean>();
   override classes = input<Partial<Record<ClassKey, string>>>();
+  displayRequiredFields = input(false);
+  documentTitle = input<string>();
+  bodyClassName = input<string>();
+  displayInfo: boolean = false;
+  displayMessage: boolean = false;
+
+  // todo add type of authenticationSelection
+  getIconCssClass(iconCssClass: string): ClassKey {
+    return iconCssClass as ClassKey;
+  }
 }

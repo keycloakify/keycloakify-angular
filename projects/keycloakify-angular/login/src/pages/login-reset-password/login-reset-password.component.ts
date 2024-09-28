e66dc732f48c7944fcf7c9d9c162dea062a6f788
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-09-28 11:33:17 +0200

    adds login-reset-password.ftl

--- projects/keycloakify-angular/login/src/pages/login-reset-password/login-reset-password.component.ts
+++ projects/keycloakify-angular/login/src/pages/login-reset-password/login-reset-password.component.ts
@@ -3,10 +3,14 @@ import { KC_CONTEXT } from 'keycloakify-angular';
 import { ClassKey } from 'keycloakify/login';
 import { KcContext } from 'keycloakify/login/KcContext';
 import { ComponentReference } from '../../classes/component-reference.class';
+import { TemplateComponent } from '../../containers/template.component';
+import { KcClassDirective } from '../../directives';
+import { KcSanitizePipe } from '../../pipes/kc-sanitize.pipe';
+import { MsgStrPipe } from '../../pipes/msg-str.pipe';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TemplateComponent, MsgStrPipe, KcClassDirective, KcSanitizePipe],
   selector: 'kc-root',
   templateUrl: 'login-reset-password.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,4 +25,9 @@ export class LoginResetPasswordComponent extends ComponentReference {
   kcContext = inject<Extract<KcContext, { pageId: 'login-reset-password.ftl' }>>(KC_CONTEXT);
   override doUseDefaultCss = input<boolean>();
   override classes = input<Partial<Record<ClassKey, string>>>();
+  displayRequiredFields = input(false);
+  documentTitle = input<string>();
+  bodyClassName = input<string>();
+  displayInfo = true;
+  displayMessage = !this.kcContext.messagesPerField.existsError('password', 'password-confirm');
 }

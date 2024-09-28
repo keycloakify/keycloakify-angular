b04924a392a4dd25a603d458e9c7254f2ae6b821
Katharina Eiserfey <42547712+kathari00@users.noreply.github.com>
2024-09-28 10:06:50 +0200

    adds login-password.ftl

--- projects/keycloakify-angular/login/src/pages/login-password/login-password.component.ts
+++ projects/keycloakify-angular/login/src/pages/login-password/login-password.component.ts
@@ -1,12 +1,17 @@
-import { ChangeDetectionStrategy, Component, forwardRef, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, inject, input, signal } from '@angular/core';
 import { KC_CONTEXT } from 'keycloakify-angular';
 import { ClassKey } from 'keycloakify/login';
 import { KcContext } from 'keycloakify/login/KcContext';
 import { ComponentReference } from '../../classes/component-reference.class';
+import { PasswordWrapperComponent } from '../../components/password-wrapper/password-wrapper.component';
+import { TemplateComponent } from '../../containers/template.component';
+import { KcClassDirective } from '../../directives/kc-class.directive';
+import { KcSanitizePipe } from '../../pipes/kc-sanitize.pipe';
+import { MsgStrPipe } from '../../pipes/msg-str.pipe';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TemplateComponent, MsgStrPipe, KcClassDirective, PasswordWrapperComponent, KcSanitizePipe],
   selector: 'kc-root',
   templateUrl: 'login-password.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,4 +26,11 @@ export class LoginPasswordComponent extends ComponentReference {
   kcContext = inject<Extract<KcContext, { pageId: 'login-password.ftl' }>>(KC_CONTEXT);
   override doUseDefaultCss = input<boolean>();
   override classes = input<Partial<Record<ClassKey, string>>>();
+  displayRequiredFields = input(false);
+  documentTitle = input<string>();
+  bodyClassName = input<string>();
+  displayInfo = false;
+  displayMessage = this.kcContext.messagesPerField.existsError('password');
+
+  isLoginButtonDisabled = signal(false);
 }

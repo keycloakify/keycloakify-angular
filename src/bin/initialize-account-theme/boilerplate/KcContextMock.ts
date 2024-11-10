5b52a8c8d14ce6fc9021a3144d0db8f49dc45ce8
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-11-10 13:35:08 +0100

    Update account boilerplate

--- src/bin/initialize-account-theme/boilerplate/KcContextMock.ts
+++ src/bin/initialize-account-theme/boilerplate/KcContextMock.ts
@@ -1,4 +1,11 @@
-import { createGetKcContextMock } from 'keycloakify/account/KcContext';
+/* eslint-disable @angular-eslint/component-class-suffix */
+import { Component, inject, OnInit, Type } from '@angular/core';
+import { provideKeycloakifyAngular } from '@keycloakify/angular/login/providers/keycloakify-angular';
+import { TemplateComponent } from '@keycloakify/angular/login/template';
+import { getKcPage } from './KcPage';
+import { getI18n } from './i18n';
+import { KC_ACCOUNT_CONTEXT } from '@keycloakify/angular/login/tokens/kc-context';
+import { createGetKcContextMock } from 'keycloakify/login/KcContext';
 import { kcEnvDefaults, themeNames } from '../kc.gen';
 import type { KcContextExtension, KcContextExtensionPerPage } from './KcContext';
 
@@ -16,3 +23,42 @@ export const { getKcContextMock } = createGetKcContextMock({
     overrides: {},
     overridesPerPage: {}
 });
+
+type StoryContextLike = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    globals: Record<string, any>;
+};
+
+export const decorators = (_: unknown, context: StoryContextLike) => ({
+    applicationConfig: {
+        providers: [
+            provideKeycloakifyAngular({
+                doUseDefaultCss: true,
+                classes: {},
+                kcContext: getKcContextMock({
+                    pageId: context.globals['pageId'],
+                    overrides: context.globals['overrides']
+                }),
+                getI18n: getI18n
+            })
+        ]
+    }
+});
+
+@Component({
+    selector: 'kc-page-story',
+    template: `@if (pageComponent) {
+        <kc-root [page]="pageComponent"></kc-root>
+    }`,
+    standalone: true,
+    imports: [TemplateComponent]
+})
+export class KcPageStory implements OnInit {
+    pageComponent: Type<unknown> | undefined;
+    kcContext = inject(KC_ACCOUNT_CONTEXT);
+    ngOnInit() {
+        getKcPage(this.kcContext.pageId).then(kcPage => {
+            this.pageComponent = kcPage.PageComponent;
+        });
+    }
+}

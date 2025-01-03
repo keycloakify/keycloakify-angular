b84be16290d9be091f0294458a28cccd40479e3c
Joseph Garrone <joseph.garrone.gj@gmail.com>
2025-01-03 23:00:46 +0100

    Update boilerplate

--- src/bin/initialize-account-theme/boilerplate/i18n.ts
+++ src/bin/initialize-account-theme/boilerplate/i18n.ts
@@ -1,11 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { i18nBuilder } from '@keycloakify/angular/account';
-import { ThemeName } from '../kc.gen';
+import type { ThemeName } from '../kc.gen';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { getI18n, ofTypeI18n } = i18nBuilder
-    .withThemeName<ThemeName>()
-    .withExtraLanguages({}) // See: https://docs.keycloakify.dev/i18n/adding-support-for-extra-languages
-    .withCustomTranslations({}) // See: https://docs.keycloakify.dev/i18n/adding-new-translation-messages-or-changing-the-default-ones
-    .build();
+/** @see: https://docs.keycloakify.dev/features/i18n */
+const { getI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+
 type I18n = typeof ofTypeI18n;
+
 export { getI18n, type I18n };

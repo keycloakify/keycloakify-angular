99672a40c72c40b7f17a07a3210e168746e69b0b
garronej <joseph.garrone@protonmail.com>
2025-03-16 01:52:38 +0100

    Fix linking script

--- src/bin/tools/getThisCodebaseRootDirPath.ts
+++ src/bin/tools/getThisCodebaseRootDirPath.ts
@@ -5,7 +5,7 @@ import * as url from 'url';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 function getThisCodebaseRootDirPath_rec(dirPath: string): string {
-    if (fs.existsSync(path.join(dirPath, 'package.json'))) {
+    if (fs.existsSync(path.join(dirPath, 'LICENSE'))) {
         return dirPath;
     }
     return getThisCodebaseRootDirPath_rec(path.join(dirPath, '..'));
